4b705949db320a72eaeb579a7c0541083c5f1d60
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-02-26 14:01:26 +0100

    Update HUDManager-ApplyPenalty.cs

--- NoCompanyPenalties/Patches/HUDManager-ApplyPenalty.cs
+++ NoCompanyPenalties/Patches/HUDManager-ApplyPenalty.cs
@@ -9,15 +9,48 @@ namespace NoCompanyPenalties.Patches
 		[HarmonyPrefix]
 		[HarmonyPriority(Priority.First)]
 		[HarmonyPatch(typeof(HUDManager), "ApplyPenalty")]
-		internal static void ApplyPenalty_postfix(HUDManager __instance, ref int playersDead, ref int bodiesInsured)
+		internal static bool ApplyPenaltyPrefix(HUDManager __instance, ref int playersDead, ref int bodiesInsured)
 		{
 			try
 			{
+				Plugin.logger.LogInfo($"Money before penalty: {MrovLib.ContentManager.Terminal.groupCredits}");
+
 				SelectableLevel currentLevel = StartOfRound.Instance.currentLevel;
 				if (MrovLib.LevelHelper.CompanyMoons.Contains(currentLevel))
 				{
 					Plugin.logger.LogDebug("Company moon detected!");
 					playersDead = 0;
+					bodiesInsured = 0;
+				}
+				else
+				{
+					Plugin.logger.LogDebug("Not a company moon");
+				}
+			}
+			catch (Exception ex)
+			{
+				Plugin.logger.LogError($"ApplyPenalty failed: {ex}");
+			}
+
+			return true;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPriority(Priority.First)]
+		[HarmonyPatch(typeof(HUDManager), "ApplyPenalty")]
+		internal static void ApplyPenaltyPostfix(HUDManager __instance, ref int playersDead, ref int bodiesInsured)
+		{
+			try
+			{
+				Plugin.logger.LogInfo($"Money after penalty: {MrovLib.ContentManager.Terminal.groupCredits}");
+
+				SelectableLevel currentLevel = StartOfRound.Instance.currentLevel;
+				if (MrovLib.LevelHelper.CompanyMoons.Contains(currentLevel))
+				{
+					Plugin.logger.LogMessage("Company moon detected!");
+
+					Plugin.logger.LogMessage("playersDead: " + playersDead);
+					Plugin.logger.LogMessage("bodiesInsured: " + bodiesInsured);
 				}
 			}
 			catch (Exception ex)
